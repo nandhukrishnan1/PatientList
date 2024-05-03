c267df003d6f105b74fb13a134f3b64d808ab851
1224 <1224@L1191>
2024-05-03 09:21:45 +0530

    Add project files.

---
+++ PatientList/Controllers/PatientController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatientList.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult PatientList()
+        {
+            return View();
+        }
+    }
+}
---
+++ PatientList/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PatientList.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
